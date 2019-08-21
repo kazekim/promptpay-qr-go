8dcac4b602ca3d13a7049317a79a32a745956cb6
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-08-21 17:57:37 +0700

    add qr image generate code

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,41 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	promptpayqr "github.com/kazekim/promptpay-qr-go"
+	"image"
+	"image/png"
+	"os"
 )
 
 func main() {
 
-	qr := promptpayqr.New()
-	amount := "420"
-	payload := qr.GeneratePayload("0899999999", &amount)
-	fmt.Println(payload)
+	qr, err := promptpayqr.QRForTargetWithAmount("0899999999","420" )
+
+	if err != nil {
+		panic(err)
+	}
+
+	byteToImage(*qr)
+
+}
+
+func byteToImage(imgByte []byte) {
+	img, _, _ := image.Decode(bytes.NewReader(imgByte))
+
+	//save the imgByte to file
+	out, err := os.Create("./QRImg.png")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = png.Encode(out, img)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
