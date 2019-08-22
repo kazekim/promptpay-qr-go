309f8b4af0d1c38e3a7d0bf7770ef464d2a100c8
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-08-22 18:24:43 +0700

    add Logo on Generated QR

--- qrgenerator.go
+++ qrgenerator.go
@@ -4,7 +4,14 @@
 
 package promptpayqr
 
-import "github.com/skip2/go-qrcode"
+import (
+	"flag"
+	"fmt"
+	"github.com/divan/qrlogo"
+	"github.com/skip2/go-qrcode"
+	"image"
+	"os"
+)
 
 func QRForTargetWithAmount(target, amount string) (*[]byte, error) {
 
@@ -34,7 +41,7 @@ func QRForBillPayment(billerID string, ref1 string, ref2 string, terminalID stri
 	return &png, nil
 }
 
-func QRForTarget(target, amount string) (*[]byte, error) {
+func QRForTarget(target string) (*[]byte, error) {
 
 	qr := New()
 	payload := qr.GeneratePayload(target, nil)
@@ -46,4 +53,37 @@ func QRForTarget(target, amount string) (*[]byte, error) {
 		return nil, err
 	}
 	return &png, nil
+}
+
+
+func QRWithPromptpayLogoForTargetWithAmount(target, amount string) (*[]byte, error) {
+
+	var (
+		input  = flag.String("promptpay", "promptpay.png", "Prompt Pay Logo")
+		size   = flag.Int("size", 256, "Image size in pixels")
+	)
+	qr := New()
+	payload := qr.GeneratePayload(target, &amount)
+
+	file, err := os.Open(*input)
+	if err != nil {
+		fmt.Println("Failed to open logo:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	logo, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("Failed to decode PNG with logo:", err)
+		os.Exit(1)
+	}
+
+	qrImage, err := qrlogo.Encode(payload, logo, *size)
+	if err != nil {
+		fmt.Println("Failed to encode QR:", err)
+		os.Exit(1)
+	}
+
+	qrBytes := qrImage.Bytes()
+	return &qrBytes, err
 }
